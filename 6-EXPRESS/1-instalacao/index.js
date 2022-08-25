032151e2ba418305cd034f80bbd4754a782821d8
Vitor Cometti <comettivitor@gmail.com>
2022-08-25 15:39:05 -0300

    Atualização

---
+++ 6-EXPRESS/1-instalacao/index.js
@@ -0,0 +1,5 @@
+const express = require('express')
+
+
+// npm init
+// npm install express
---
+++ 6-EXPRESS/6-param-url/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+
+const app = express()
+const port = 3000 //Variavel ambiente
+
+const path = require('path')
+const basePath = path.join(__dirname, 'templates')
+
+const checkAuth = function(req, res, next){ //next usado para dar prosseguimento nas funções
+    req.authStatus = true //verifica usuário autenticado ou não
+    if(req.authStatus){
+        console.log('está logado!')
+        next()
+    }else{
+        console.log('Não está logado! faça login...')
+        next()
+    }
+}
+
+app.use(checkAuth)
+
+//Cria rota
+//função anonima que envia resposta ao usuário
+app.get('/users/:id', (req, res) =>{
+    const id = req.params.id
+
+    //leitura da tabela users, resgatando um usuário
+
+    console.log(`buscando usuario ${id}`)
+    res.sendFile(`${basePath}/users.html`)
+})
+
+app.get('/', (req, res) =>{
+    res.sendFile(`${basePath}/index.html`)
+})
+
+//função para quem executa o sistema, quem é o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`)
+})
